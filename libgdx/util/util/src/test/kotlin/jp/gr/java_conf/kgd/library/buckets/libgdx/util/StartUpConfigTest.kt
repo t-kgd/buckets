617db71dad8d97e92f39eb9a9bc239447fe34fff
misakura <t-kgd@users.noreply.github.com>
2015-09-25 11:07:14 +0900

    change フォルダを移動。

---
+++ libgdx/util/util/src/test/kotlin/jp/gr/java_conf/kgd/library/buckets/libgdx/util/StartUpConfigTest.kt
@@ -0,0 +1,45 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015 Misakura.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jp.gr.java_conf.kgd.library.buckets.libgdx.util
+
+import org.junit.Test
+import java.io.InputStream
+import kotlin.test.assertEquals
+
+public class StartUpConfigTest {
+
+    @Test
+    fun defaultImplTest() {
+        val sut = StartUpConfig.DefaultStartUpConfig()
+        val resourceLoaderMock: (String) -> InputStream = { javaClass.classLoader.getResourceAsStream(it) }
+        sut.resourceLoader = resourceLoaderMock
+        sut.reload()
+
+        assertEquals("resources/", sut.resourcesPath)
+        assertEquals("scripts/", sut.scriptsPath)
+        assertEquals("save/", sut.savePath)
+        assertEquals("resources/ui/uiskin.json", sut.defaultSkinPath)
+    }
+}
